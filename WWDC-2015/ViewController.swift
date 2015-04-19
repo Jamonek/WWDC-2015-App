3ecac4f220a980099d7bb535094104e1a2d88531
Jamone Kelly <xg.monie089@gmail.com>
2015-04-19 00:16:13 -0400

    Initial Commit

---
+++ WWDC-2015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWDC-2015
+//
+//  Created by Jamone Alexander Kelly on 4/19/15.
+//  Copyright (c) 2015 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
